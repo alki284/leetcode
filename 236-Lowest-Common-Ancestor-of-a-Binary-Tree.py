8ba53bef69b39dc9ccd98b541f4cae3402d61854
Alexander Unsworth-Tomlinson <45341802+alki284@users.noreply.github.com>
2022-06-09 18:32:09 +0100

    Simpler code, similar to one in NeetCode video

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree.py
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree.py
@@ -8,22 +8,9 @@
 class Solution:
     res = None
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or not p or not q: return None
-        
-        def search(root, p, q):
-            if not root: return False
-            mid = left = right = False
-            if root.val == p.val or root.val == q.val: 
-                mid = True
-            
-            left = search(root.left, p, q)
-            right = search(root.right, p, q)
-            if mid:
-                if left or right:
-                    self.res = root
-            elif left and right:
-                self.res = root
-            return mid or left or right
-            
-        search(root, p, q)
-        return self.res
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
